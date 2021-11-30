23eae267d9a217770b759dd6aaef0ec52909eaf3
Leonardo Diegues <dscleo@gmail.com>
2021-11-30 13:40:02 -0300

    Corrige data das manchetes para viabilizar teste

--- lidebot/tests/test_main.py
+++ lidebot/tests/test_main.py
@@ -7,26 +7,26 @@ test_headlines = [
     Headline(
         title="a very very very long title1",
         url="url1",
-        collected_at=datetime.strptime("2021-01-01 20:30:10", "%Y-%m-%d %H:%M:%S"),
+        collected_at=datetime.strptime("2021-01-01 20:30:10.3242", "%Y-%m-%d %H:%M:%S.%f"),
         source="folha",
     ),
     Headline(
         title="a very very very long title2",
         url="url2",
-        collected_at=datetime.strptime("2021-01-01 20:30:10", "%Y-%m-%d %H:%M:%S"),
+        collected_at=datetime.strptime("2021-01-01 20:30:10.3242", "%Y-%m-%d %H:%M:%S.%f"),
         source="estadao",
     ),
     Headline(
         title="a very very very long title3",
         url="url3",
-        collected_at=datetime.strptime("2021-01-01 20:30:10", "%Y-%m-%d %H:%M:%S"),
+        collected_at=datetime.strptime("2021-01-01 20:30:10.3242", "%Y-%m-%d %H:%M:%S.%f"),
         source="oglobo",
     ),
 ]
 
 
 def test_load_headlines() -> None:
-    headlines = load_headlines("lidebot/tests/test_data/test_headlines.txt")
+    headlines = load_headlines("lidebot/tests/test_data")
     assert headlines == test_headlines
 
 
