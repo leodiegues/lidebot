5fc227210a5cb2ccffd7eede8d6c11cbcce981e1
Leonardo Diegues <dscleo@gmail.com>
2022-01-27 21:02:55 -0300

    Adiciona scraper do Estadão

--- lidebot/crawler/spiders/estadao.py
+++ lidebot/crawler/spiders/estadao.py
@@ -10,10 +10,11 @@ class EstadaoSpider(scrapy.Spider):
     start_urls = ["http://estadao.com.br/"]
 
     def parse(self, response):
-        headline_tag = response.css("div.intro:nth-of-type(1) > a")
+        headline_tag = response.css("div.intro")[0].css("a:nth-of-type(1)")
+        print(headline_tag)
         yield Headline(
-            title=headline_tag.css("::text").extract(),
-            url=headline_tag.css("::attr(href)").extract(),
+            title=headline_tag.css("::attr(title)")[0].extract(),
+            url=headline_tag.css("::attr(href)")[0].extract(),
             collected_at=datetime.now(),
             source=self.name,
         )
